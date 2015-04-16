0daf8a577df855c969c2a7171019dc7cba33bca3
Ezra <ezrazigmond@gmail.com>
2015-04-15 21:09:12 -0400

    Finished inversions function

--- npuzzle/src/npuzzles/puzzle.clj
+++ npuzzle/src/npuzzles/puzzle.clj
@@ -1,3 +1,22 @@
+; Puzzle record has integer number of rows, columsns
+; and an row * column length vector of tiles [0, row * column)
+(defrecord Puzzle [rows columns tiles])
+
+(defn random-puzzle
+	"Given a number of rows and columns, creates random puzzle"
+	[r c]
+	{:rows r, :cols c, :tiles (shuffle (range (* r c)))})
+
+(defn inversions
+	"Given a puzzle, returns the number of invertions"
+	[{tiles :tiles}]
+	(loop [inv 0
+		   lst (remove #(= % 0) tiles)]
+		(if (empty? lst)
+			inv
+			(let [hd (first lst) tl (rest lst)]
+				(recur (+ inv (count (filter #(< % hd) tl))) tl)))))
+
 (defn swap
 	"Given a vector and two indices, swaps the values"
 	[v i1 i2]
