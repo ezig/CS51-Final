2a8496f462cae233ff81b7694fe43874a29cdcb2
Liam James Mulshine <ljmulshine@Liams-MacBook-Pro.local>
2015-04-20 16:37:32 -0400

    Mapped out what we still need to do for astar implementation

--- npuzzle/src/npuzzles/astar.clj
+++ npuzzle/src/npuzzles/astar.clj
@@ -11,7 +11,7 @@
 (defrecord TreePuzzle [puzzle parent g h])
 
 
-
+;PRIVATE FUNCTIONS
 (defn- puzzle-to-tree 
 	[puzzle depth parent]
 	(let [distance (+ depth (puzzle/manhattan-distance puzzle))]
@@ -60,4 +60,28 @@
 			newqueue
 		    (let [initial (first puzzles) others (rest puzzles)]
 			(recur others (insert-queue initial newqueue))))))
+; TODO 
+(defn- dequeue
+	"Returns the first item (lowest cost, highest priority TreePuzzle) off the queue."
+	[pqueue])
+
+(defn- step 
+	"Dequeues a puzzle off the priority queue, checks if it is equal to the
+	goal state (by calling solved?). If it is, return the TreePuzzle; if not, insert the child
+	nodes into the priority queue and recursively pass the new priority queue into step.
+	Thus, step will always return a TreePuzzle that corresponds to the goal state."
+	[pqueue])
+
+(defn- map-solution
+	"Given the return value of solve (a TreePuzzle node thatcorresponds to the goal state), 
+	the function returns a list of Puzzles that correspond to the optimal path found by A* 
+	by retracing the path back to the starting node (through the Parent nodes of each
+    TreePuzzle until you reach the node with the nil parent)."
+	[tree-puzzle])
+
+; PUBLIC FUNCTIONS
+(defn solve
+	"Given a initial puzzle state, returns the sequence of puzzles needed to go from that 
+	puzzle state to the goal state."
+	[puzzle])
       
