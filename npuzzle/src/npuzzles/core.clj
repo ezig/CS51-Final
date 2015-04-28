5bd4bf07401c4ccc6c8eb69b6482d97d3a7d0a81
Eric Chan <ecphylo@gmail.com>
2015-04-28 15:04:29 -0400

    core main

--- npuzzle/src/npuzzles/core.clj
+++ npuzzle/src/npuzzles/core.clj
@@ -4,7 +4,7 @@
 	(:require [npuzzles.genetic :as genetic])
   	(:require [npuzzles.heap :as heap])
   	(:gen-class))
-
+(comment
 (defn -main
 	 "I don't do a whole lot ... yet."
  	[& args]
@@ -29,4 +29,21 @@
  	(println "Under Cow-struction")
   (let [x (puzzle/gen-puzzle 2)]
     (map (fn [x] (print-str (puzzle/to-string x))) (astar/solve x))
-  ))
+  )))
+(defn main
+	"commandline interaction with our program"
+	[type trials height width]
+	(cond 
+		(re-matches #"(?i)genetic" type) 
+		do((loop [cnt trials]
+			(let [x (puzzle/gen-puzzle height width)]
+				(map (fn [x] (print-str (puzzle/to-string x))) (genetic/solve x))
+			(recur (- cnt 1)))))
+		(re-matches #"(?i)astar" type) 
+		do((loop [cnt trials]
+			(let [x (puzzle/gen-puzzle height width)]
+				(map (fn [x] (print-str (puzzle/to-string x))) (astar/solve x))
+			(recur (- cnt 1)))))
+		:else "input format should be main (solver type) (# of trials)"
+		)
+	)
