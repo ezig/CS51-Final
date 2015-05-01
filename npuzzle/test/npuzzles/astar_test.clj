bba0a584e5ad4f5b8ef9183b519ecaa8e15e1c15
Liam James Mulshine <ljmulshine@Liams-MacBook-Pro.local>
2015-05-01 12:21:38 -0400

    Fixed puzzle-to-tree test

--- npuzzle/test/npuzzles/astar_test.clj
+++ npuzzle/test/npuzzles/astar_test.clj
@@ -10,13 +10,13 @@
     (def puzzle-to-tree #'astar/puzzle-to-tree) 
 	(testing "puzzle-to-tree")
 	(let [p1 (puzzle/gen-puzzle 3 3 [1 2 3 4 5 6 7 0 8])
-	      parent1 (puzzle/gen-puzzle 3 3 [1 2 3 4 0 6 7 5 8])
+	      parent1 (puzzle-to-tree (puzzle/gen-puzzle 3 3 [1 2 3 4 0 6 7 5 8]) 0 nil puzzle/manhattan-distance)
 	      p2 (puzzle/gen-puzzle 4 4 [15 1 2 3 4 5 6 7 8 9 10 11 0 13 14 12])
-	      parent2 (puzzle/gen-puzzle 4 4 [15 1 2 3 4 5 6 7 0 9 10 11 8 13 14 12])]
+	      parent2 (puzzle-to-tree (puzzle/gen-puzzle 4 4 [15 1 2 3 4 5 6 7 0 9 10 11 8 13 14 12]) 0 nil puzzle/manhattan-distance)]
 	 (is (= (puzzle-to-tree p1 1 parent1 puzzle/manhattan-distance) 
 	 	{:puzzle p1, :parent parent1, :g 1, :h 2}))
-	 (is (= (puzzle-to-tree p2 5 parent2 puzzle/manhattan-distance) 
-	 	{:puzzle p2, :parent parent2, :g 5, :h 31}))))
+	 (is (= (puzzle-to-tree p2 1 parent2 puzzle/manhattan-distance) 
+	 	{:puzzle p2, :parent parent2, :g 1, :h 26}))))
 
 (deftest gen-children-test
 	(def gen-children #'astar/gen-children) 
@@ -37,31 +37,3 @@
     	                                   :parent {:puzzle {:rows 3, :cols 3, :tiles [1 2 3 4 5 6 7 0 8]}, 
     	                                   :parent {:puzzle {:rows 3, :cols 3, :tiles [1 2 3 4 0 6 7 5 8]}, 
     	                                   :parent nil, :g 0, :h 2}, :g 1, :h 2}, :g 2, :h 4}]))))))
-
-(deftest insert-children-test 
-	(def insert-children #'astar/insert-children) 
-	(def puzzle-to-tree #'astar/puzzle-to-tree)
-	(testing "insert-children")
-	(let [p1 (puzzle/gen-puzzle 3 3 [1 2 3 4 5 6 7 0 8])
-	      parent1 (puzzle/gen-puzzle 3 3 [1 2 3 4 0 6 7 5 8])
-	      p2 (puzzle/gen-puzzle 3 3 [1 2 3 4 0 6 7 5 8])
-	      parent2 (puzzle/gen-puzzle 3 3 [1 0 3 4 2 6 1 5 8])
-	      p3 (puzzle/gen-puzzle 3 3 [1 2 3 4 5 6 7 8 0])
-	      parent3 (puzzle/gen-puzzle 3 3 [1 2 3 4 5 0 7 8 6])
-	      p4 (puzzle/gen-puzzle 3 3 [1 2 0 4 5 3 7 8 6])
-	      parent4 (puzzle/gen-puzzle 3 3 [1 2 3 4 5 0 7 8 6])]
-	(let [p1-parent (puzzle-to-tree parent1 0 nil puzzle/manhattan-distance)
-		  p2-parent (puzzle-to-tree parent2 0 nil puzzle/manhattan-distance)
-		  p3-parent (puzzle-to-tree parent3 0 nil puzzle/manhattan-distance)
-		  p4-parent (puzzle-to-tree parent4 0 nil puzzle/manhattan-distance)]
-    (let [p1-tree (puzzle-to-tree p1 1 p1-parent puzzle/manhattan-distance)
-    	  p2-tree (puzzle-to-tree p2 1 p2-parent puzzle/manhattan-distance)
-    	  p3-tree (puzzle-to-tree p3 1 p3-parent puzzle/manhattan-distance)
-    	  p4-tree (puzzle-to-tree p4 1 p4-parent puzzle/manhattan-distance)]
-    (let [pqueue (pmap/priority-map [1 2 3 4 5 6 7 0 8] p1-tree [ 1 2 3 4 0 6 7 5 8] p2-tree) 
-    	  othertrees [p3-tree p4-tree]
-    	  closed (pmap/priority-map)] 
-    (let [inserted-puzzles (pmap/priority-map [1 2 3 4 5 6 7 8 0] p3-tree [1 2 3 4 5 6 7 0 8] p1-tree
-     										  [1 2 3 4 0 6 7 5 8] p2-tree [1 2 3 4 5 0 7 8 6] p4-tree)
-    	  actual (insert-children othertrees pqueue closed)]
-    	  (is (= actual inserted-puzzles))))))))
